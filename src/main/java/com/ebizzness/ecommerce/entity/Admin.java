c3f3389ca4c29d3dc42939bab20b929aca8c31a3
Fatimah Najihah <fatimahnajihah05@gmail.com>
2026-06-09 17:39:15 +0800

    finalised userAuth

--- src/main/java/com/ebizzness/ecommerce/entity/Admin.java
+++ src/main/java/com/ebizzness/ecommerce/entity/Admin.java
@@ -1,10 +1,16 @@
 package com.ebizzness.ecommerce.entity;
 
-import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
-@DiscriminatorValue("ADMIN")
+@Table(name = "admin")
+@PrimaryKeyJoinColumn(name = "admin_id") // Maps admin_id to user_id
 public class Admin extends User {
-    
+    // Inherits everything cleanly from User
 }
